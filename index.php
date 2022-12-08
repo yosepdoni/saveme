51b313752ab44aa52250e9e1a2d7c7824895b9db
root <root@anonymous.anonymous>
2022-12-09 05:23:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Tingbers</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php
+  include 'header.php';
+  ?>
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animated fadeInDown">Selamat datang di <span>Tingbers</span></h2>
+              <p class="animated fadeInUp">Jasa pembuatan website dan jasa SEO</p>
+              <a href="#layanan" class="btn-get-started animated fadeInUp scrollto">Read More</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section>
+  <!-- End Hero -->
+  <!-- ======= Main ======= -->
+  <?php
+  include 'main.php';
+  ?>
+  <!-- End main -->
+  <!-- ======= footer ======= -->
+  <?php
+  include 'footer.php';
+  ?>
+  <!-- ======= Footer ======= -->
+ <!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ main.php
@@ -0,0 +1,97 @@
+<main id="main">
+    <section id="layanan" class="services">
+      <div class="container-fluid">
+        <div class="section-title">
+          <h3>Jasa pembuatan website Kalimantan Barat </h3>
+          <div class="row justify-content-center">
+            <p>Sengitnya persaingan bisnis di era digital mendorong organisai untuk saling berlomba menampilkan inovasi dalam berbagai ide promosi.karena hal tersebut, website menjadi salah satu pilihan media untuk pengembangan bisnis. <br>
+            Sebagai partner bisnis  penyedia jasa pembuatan website Pontianak, kami terus berupaya untuk menjadi yang terbaik untuk memberikan performa website yang optimal.
+            </p>
+          </div>
+        </div>
+          <div class="section-title">
+            <h3>Wujudkan website sesuai kebutuhan Anda</h3>
+            <div class="row justify-content-center">
+              <p>
+                Dalam kebutuhan bisnis Anda, kami akan memberikan solusi terbaik di setiap proses tahapannya. Berikut merupakan layanan yang kami sediakan:
+              </p>
+              <div class="row justify-content-between">
+                <p><b>E-Commerce</b><br>
+                   <b>Yayasan / Pendidikan / Perusahaan</b>
+                </p>
+                <p><b>Personal</b><br>
+                   <b>Bisnis</b>
+                </p>
+              </div>
+            </div>
+          </div>
+        <div class="section-title">
+          <h3>Mengapa membuat website di Tingbers?</h3>
+          <p>Kami menerapkan proses standar yang tinggi dalam setiap proses, didukung dengan keahlian yang kami miliki untuk menciptakan hasil yang optimal bagi website Anda.</p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+               <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-line-chart-line"></i></div>
+                <h4 class="title"><a href="">SEO Friendly</a></h4>
+                <p class="description">Memudahkan website muncul dalam pencarian google</p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-settings-2-line"></i></div>
+                <h4 class="title"><a href="">User Friendly</a></h4>
+                <p class="description">Memudahkan pengguna untuk mengoperasikan atau melakukan aksi berdasarkan fitur yang tersedia </p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-lock-line"></i></div>
+                <h4 class="title"><a href="">Performa & Keamanan </a></h4>
+                <p class="description">Menerapkan proses dengan standar yang tinggi, mulai dari tahap perencanaan hingga pengujian untuk menjamin performa dan keamanan website.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+    <hr>
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about"  style="background-image: url(assets/img/slide/tentang.svg)">
+      <div class="container-fluid ">
+
+        <div class="row justify-content-center">
+          <div class="col-xl-5 col-lg-6 d-flex justify-content-center align-items-stretch position-relative">
+            <img src="https://i.ibb.co/XfBph9y/logologin.png" style="width: 500px; height: 400px;" /> 
+          </div>
+
+          <div class="col-xl-7 col-lg-2 icon-boxes d-flex flex-column align-items-stretch justify-content-center px-lg-2">
+            <h3>Tentang Kami</h3>
+            <p>Kami adalah Web Development Team yang menyediakan jasa pembuatan website dan jasa SEO serta beralokasi di Pontianak, Kalimantan Barat. <br>
+            kami dapat membantu Anda untuk membangun website dengan harga terjangkau mulai dari harga Rp500.000,00 - Rp3000.000,00.</p>
+
+            <p><b>Ketahui apa itu website?</b><br>Website adalah kumpulan halaman yang menyajikan informasi tertentu dan dapat diakses dengan mudah oleh siapapun, kapanpun, dan di manapun melalui internet.</p>
+            <p><b>Apa itu SEO?</b><br>SEO adalah singkatan dari “search engine optimization.” Teknik yang dilakukan untuk mengoptimasi website supaya mendapat ranking teratas di hasil pencarian Google, situs juga akan mudah orang ditemukan</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="text-center">
+          <h3>Call To Action</h3>
+          <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a class="cta-btn" href="#">Call To Action</a>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+
+
+  </main>
